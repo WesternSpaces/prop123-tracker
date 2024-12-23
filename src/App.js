e75881537d81950bc56de6f6b8160350496354f6
WesternSpaces <sarah@westernspaces.co>
2024-12-23 10:07:08 -0700

    Add main App component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Prop123Tracker from './components/Prop123Tracker';
+
+function App() {
+  return (
+    <div className="App">
+      <Prop123Tracker />
+    </div>
+  );
+}
+
+export default App;
